58101608425900fa8efc66b847b65af2014646df
Lucas da Silveira <lucas.hoffmann.lh@gmail.com>
2020-06-09 16:50:23 -0300

    initial merge

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,19 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const exists = await this.customersRepository.findByEmail(email);
+    if (exists) {
+      throw new AppError('Costumer already exists');
+    }
+
+    const costumer = await this.customersRepository.create({ email, name });
+    return costumer;
   }
 }
 
--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -17,16 +17,71 @@ interface IRequest {
   products: IProduct[];
 }
 
+interface IProductData {
+  product_id: string;
+  quantity: number;
+  price: number;
+}
+
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customerExists = await this.customersRepository.findById(customer_id);
+    if (!customerExists) {
+      throw new AppError('Customer does not exist');
+    }
+
+    const productsId = products.map(product => ({ id: product.id }));
+
+    const productsData = await this.productsRepository.findAllById(productsId);
+
+    const updatedQuantity: IProduct[] = [];
+    const newProducts: IProductData[] = [];
+
+    products.forEach(product => {
+      const data = productsData.find(
+        productData => productData.id === product.id,
+      );
+
+      if (!data) {
+        throw new AppError('produc not found');
+      }
+
+      if (product.quantity > data.quantity) {
+        throw new AppError('Quantity not disponible');
+      }
+
+      updatedQuantity.push({
+        quantity: data.quantity - product.quantity,
+        id: product.id,
+      });
+
+      newProducts.push({
+        product_id: product.id,
+        quantity: product.quantity,
+        price: data.price,
+      });
+    });
+
+    await this.productsRepository.updateQuantity(updatedQuantity);
+
+    const order = await this.ordersRepository.create({
+      customer: customerExists,
+      products: newProducts,
+    });
+
+    return order;
   }
 }
 
